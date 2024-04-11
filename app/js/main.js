b94b360812236b2ecf33d4e69695ddf16a8b13c5
otsairaze <ssavelevdenchikk@gmail.com>
2024-04-11 23:21:01 +0300

    feat: make adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,13 @@
-// Ваш JavaScript
+const btn = document.querySelector("#btn");
+const overlay = document.querySelector("#over");
+const popup = document.querySelector(".popup");
+
+btn.onclick = function () {
+  overlay.classList.add("popup--active");
+  overlay.onclick = function () {
+    overlay.classList.remove("popup--active");
+  };
+  popup.onclick = function (e) {
+    e.stopPropagination();
+  };
+};
