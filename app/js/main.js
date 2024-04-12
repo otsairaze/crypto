7f3a7beb3b56b9ed15bb367bba3b7faecdda610b
otsairaze <ssavelevdenchikk@gmail.com>
2024-04-12 21:44:22 +0300

    feat: add popup

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,27 @@
-const btn = document.querySelector("#btn");
+const btn = document.querySelectorAll("#btn");
 const overlay = document.querySelector("#over");
 const popup = document.querySelector(".popup");
+const body = document.querySelector("body");
+const btnScroll = document.querySelector(".main__second-btn");
+const program = document.querySelector(".program");
 
-btn.onclick = function () {
-  overlay.classList.add("popup--active");
-  overlay.onclick = function () {
-    overlay.classList.remove("popup--active");
-  };
-  popup.onclick = function (e) {
-    e.stopPropagination();
-  };
+overlay.onclick = function () {
+  overlay.classList.remove("popup--active");
+  body.style.overflowY = "auto";
 };
+popup.addEventListener("click", (e) => {
+  e.stopPropagation();
+});
+
+btnScroll.onclick = function () {
+  const positionY = program.getBoundingClientRect().y;
+  window.scrollTo(0, positionY);
+};
+
+btn.forEach(
+  (element) =>
+    (element.onclick = function () {
+      overlay.classList.add("popup--active");
+      body.style.overflow = "hidden";
+    })
+);
